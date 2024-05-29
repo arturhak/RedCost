8123fdf57cd27c0b876ba02a1c84240de2bec062
AvanesovVova <vova.avanesov.ads@gmail.com>
2024-05-29 19:59:59 +0400

    roadmap

--- src/components/RoadmapWeb.tsx
+++ src/components/RoadmapWeb.tsx
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
-function RoadmapWeb () {
+
+
+function swap() {
+  const elementFirst = document.getElementById("first");
+    if (elementFirst) {
+      // Trigger the transition by adding the 'move' class
+      elementFirst.classList.add("moveLeft");
+    }
+
+    const elementSecond = document.getElementById("second");
+    if (elementSecond) {
+      // Trigger the transition by adding the 'move' class
+      elementSecond.classList.add("moveStart");
+    }
+   
+    const elementThird = document.getElementById("third");
+    if (elementThird) {
+      // Trigger the transition by adding the 'move' class
+      elementThird.classList.add("move");
+    }
+
+    const elementForth = document.getElementById("fourth");
+    if (elementForth) {
+      // Trigger the transition by adding the 'move' class
+      elementForth.classList.add("moveRight");
+    }
+
+
+    const elementFourth = document.getElementById("forth");
+    if (elementFourth) {
+        // Trigger the transition by adding the 'move' class
+        elementFourth.classList.add("moveRight");
+      }
+
+      const elementFifth = document.getElementById("fifth");
+      if (elementFifth) {
+          // Trigger the transition by adding the 'move' class
+          elementFifth.classList.add("moveDown");
+        }
+}   
+
+function RoadmapWeb() {
     return (
-        <>
-            <button>NEXT</button>
-        <div className="roadmapWeb">
-            {["first","second","third","fourth","fifth"].map((el:any,index)=> <div className="r_1" key={index} id={el}>{el}</div> )}
-        </div>
-        </>
+        
+      <div className="roadmapWeb">
+              <button onClick={swap}>NEXT</button>
+                <div id="first">first</div>
+                <div id="second">second</div>
+                <div id="third">third</div>
+                <div id="fourth">fourth</div>
+                <div id="fifth">fifth</div>
+            </div>
     )
 }
 
