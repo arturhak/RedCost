d15107b0d4c678c3ddeccdd4f409285e99948437
arturhak <arturhak08@gmail.com>
2024-06-02 20:46:00 +0400

    dlfkjsd;lf

--- src/index.tsx
+++ src/index.tsx
@@ -35,34 +35,34 @@ const handleScroll = () => {
 
 
     // el.forEach((el:any)=>{
-        let position = el.getBoundingClientRect();
-        let position1 = el1.getBoundingClientRect();
-        let position2 = el2.getBoundingClientRect();
+        let position:any = el?.getBoundingClientRect();
+        let position1:any = el1?.getBoundingClientRect();
+        let position2:any = el2?.getBoundingClientRect();
 
-        if (position.top + 600< window.innerHeight && position.bottom > 0) {    //position.top + 100
-            elLeft.classList.add("anim-from-left")
-            elRight.classList.add("anim-from-right")
+        if (position?.top + 600< window.innerHeight && position?.bottom > 0) {    //position.top + 100
+            elLeft?.classList.add("anim-from-left")
+            elRight?.classList.add("anim-from-right")
             // el.classList.add("visible")
         }
         else {
-            elLeft.classList.remove("anim-from-left")
-            elRight.classList.remove("anim-from-right")
+            elLeft?.classList.remove("anim-from-left")
+            elRight?.classList.remove("anim-from-right")
             // el.classList.remove("visible")
         }
 
-    if (position1.top + 600 < window.innerHeight && position1.bottom > 0) {
+    if (position1?.top + 600 < window.innerHeight && position1?.bottom > 0) {
         elLeft1.classList.add("anim-from-left")
         elRight1.classList.add("anim-from-right")
         // el1.classList.add("visible")
     }
     else {
-        elLeft1.classList.remove("anim-from-left")
-        elRight1.classList.remove("anim-from-right")
+        elLeft1?.classList.remove("anim-from-left")
+        elRight1?.classList.remove("anim-from-right")
         // el1.classList.remove("visible")
     }
 
-    if (position2.top + 200 < window.innerHeight && position2.bottom > 0) {
-        el2.classList.add("visible-roadmap")
+    if (position2?.top + 200 < window.innerHeight && position2?.bottom > 0) {
+        el2?.classList.add("visible-roadmap")
     }
     else {
         // el2.classList.remove("visible-roadmap")
--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -5,10 +5,13 @@ import CountDown from "../components/CountDown";
 import MainComponent from "../components/MainComponent";
 import RedCost from "../assets/images/red-cost.svg";
 import RedCostMob from "../assets/images/red-cost-mob.svg";
+import RedCostCenter from "../assets/images/red-cost-center.svg";
 import Security from "../components/Security";
 import SecurytyImage from "../assets/images/security-logo.svg";
 import RoadmapWeb from "../components/RoadmapWeb";
 import {socialItemsList} from "../constants/socialItems";
+import SecurityDiscover from "../components/SecurityDiscover";
+import MainComponent2 from "../components/MainComponent2";
 
 function About () {
 
@@ -24,18 +27,40 @@ function About () {
                     Discover
                 </p>
 
-                <div className="digital-art">
-                    <div className="header-side">
+                <div className="discover-digital-art">
+                    <div className="discover-header-side">
                         Enter The New World<br/>of Digital Art
                     </div>
-                    <div className="bottom-side">
-                        <div className="bottom-side-left">
-                            <div className="bottom-side-left-header">
+                    <div className="discover-bottom-side">
+                        <div className="discover-bottom-side-left">
+                            <div className="discover-bottom-side-left-header">
                                 The marketplace for those who know<br/>the true value of digital assets.
                             </div>
-                            <div className="bottom-side-left-header_buttons">
-                                <div className="left-button">Explore now</div>
-                                <div className="right-button">AirDrop</div>
+                            <div className="discover-bottom-side-left-header_buttons">
+                                <div className="discover-left-button">Whitepaper</div>
+                                <div className="discover-right-button">About</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="discover-first-item-mobile">
+                <p className="discover-text">
+                    Discover
+                </p>
+
+                <div className="discover-digital-art">
+                    <div className="discover-header-side">
+                        Enter The New World<br/>of Digital Art
+                    </div>
+                    <div className="discover-bottom-side">
+                        <div className="discover-bottom-side-left">
+                            <div className="discover-bottom-side-left-header">
+                                The marketplace for those who know<br/>the true value of digital assets.
+                            </div>
+                            <div className="discover-bottom-side-left-header_buttons">
+                                <div className="discover-left-button">Whitepaper</div>
+                                <div className="discover-right-button">About</div>
                             </div>
                         </div>
                     </div>
@@ -44,12 +69,12 @@ function About () {
             <div className="security-component">
                 <div className="security-component-header">
                     <div className="line_1"></div>
-                    <div className="security-component-title">we offer you</div>
+                    <div className="security-component-title">Title</div>
                     <div className="line_1"></div>
                 </div>
-                <div className="security-group">
-                    {[1,2,3,4].map((index)=> {
-                        return <Security
+                <div className="discover-security-group">
+                    {[1,2,3,4,5,6].map((index)=> {
+                        return <SecurityDiscover
                             key={index}
                             image={SecurytyImage}
                             subtitle="Security"
@@ -62,45 +87,7 @@ function About () {
                 </div>
             </div>
 
-            <div className="roadmap">
-
-                <RoadmapWeb />
-
-            </div>
-            <div className="lastMainComponent">
-                <MainComponent
-                    image={RedCost}
-                    imageMobile={RedCostMob}
-                    title={true}
-                    titleText=""
-                    subtitle="TiTle"
-                    content1={"RED COST is the official marketplace for valuing, buying and selling digital assets (NFT). Complies with all current cryptocurrency security requirements. All transactions are legal and transparent."}
-                    content2={"To create a safe and convenient platform for everyone who understands the topic and wants to earn safely on NFT."}
-                    reverse={true}
-                    buttonSize={true}
-                    isInput={false}
-                    buttonValue="CONTACT"
-                />
-            </div>
-            <div className="social-big-logo-group">
-                <div className="security-component-header">
-                    <div className="line_1"></div>
-                    <div className="security-component-title">follow our socials</div>
-                    <div className="line_1"></div>
-                </div>
-                <div className="social-group-component">
-                    {socialItemsList.map((item,key)=> {
-                        return <div
-                            className="folow-item"
-                            key={key}
-                            onClick={() => handleClickSocialItem(item.name)}
-                        >
-                            {item.logosvg}
-                        </div>
-                    })}
-                </div>
-            </div>
-            <div className="lastMainComponent1">
+            {/*<div className="lastMainComponent1">*/}
                 <MainComponent
                     image={RedCost}
                     imageMobile={RedCostMob}
@@ -112,10 +99,78 @@ function About () {
                     reverse={false}
                     buttonSize={true}
                     isInput={false}
-                    buttonValue="WHITEPAPER"
+                    buttonValue="Button"
                 />
-            </div>
+            {/*</div>*/}
 
+            <MainComponent2
+                image={RedCostCenter}
+                imageMobile={RedCostMob}
+                subtitle="TiTle"
+                content1={"RED COST is the official marketplace for valuing, buying and selling digital assets (NFT). Complies with all current cryptocurrency security requirements. All transactions are legal and transparent."}
+                content2={"To create a safe and convenient platform for everyone who understands the topic and wants to earn safely on NFT."}
+                content11={"RED COST is the official marketplace for valuing, buying and selling digital assets (NFT). Complies with all current cryptocurrency security requirements. All transactions are legal and transparent."}
+                buttonSize={false}
+                buttonValue="Button"
+            />
+
+            {/*<div className="lastMainComponent1">*/}
+                <MainComponent
+                    image={RedCost}
+                    imageMobile={RedCostMob}
+                    title={true}
+                    titleText=""
+                    subtitle="TiTle"
+                    content1={"RED COST is the official marketplace for valuing, buying and selling digital assets (NFT). Complies with all current cryptocurrency security requirements. All transactions are legal and transparent."}
+                    content2={"To create a safe and convenient platform for everyone who understands the topic and wants to earn safely on NFT."}
+                    reverse={true}
+                    buttonSize={true}
+                    isInput={false}
+                    buttonValue="Button"
+                />
+            {/*</div>*/}
+            {/*<div className="lastMainComponent1">*/}
+                <MainComponent
+                    image={RedCost}
+                    imageMobile={RedCostMob}
+                    title={true}
+                    titleText=""
+                    subtitle="TiTle"
+                    content1={"RED COST is the official marketplace for valuing, buying and selling digital assets (NFT). Complies with all current cryptocurrency security requirements. All transactions are legal and transparent."}
+                    content2={"To create a safe and convenient platform for everyone who understands the topic and wants to earn safely on NFT."}
+                    reverse={false}
+                    buttonSize={true}
+                    isInput={false}
+                    buttonValue="Button"
+                />
+            {/*</div>*/}
+
+            <MainComponent2
+                image={RedCostCenter}
+                imageMobile={RedCostMob}
+                subtitle="TiTle"
+                content1={"RED COST is the official marketplace for valuing, buying and selling digital assets (NFT). Complies with all current cryptocurrency security requirements. All transactions are legal and transparent."}
+                content2={"To create a safe and convenient platform for everyone who understands the topic and wants to earn safely on NFT."}
+                content11={"RED COST is the official marketplace for valuing, buying and selling digital assets (NFT). Complies with all current cryptocurrency security requirements. All transactions are legal and transparent."}
+                buttonSize={true}
+                buttonValue="Button"
+            />
+
+            {/*<div className="lastMainComponent1">*/}
+            <MainComponent
+                image={RedCost}
+                imageMobile={RedCostMob}
+                title={true}
+                titleText=""
+                subtitle="TiTle"
+                content1={"RED COST is the official marketplace for valuing, buying and selling digital assets (NFT). Complies with all current cryptocurrency security requirements. All transactions are legal and transparent."}
+                content2={"To create a safe and convenient platform for everyone who understands the topic and wants to earn safely on NFT."}
+                reverse={true}
+                buttonSize={true}
+                isInput={false}
+                buttonValue="Button"
+            />
+            {/*</div>*/}
         </div>
     )
 }
