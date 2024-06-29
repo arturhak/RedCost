de8595890aa4d62e923d656a801c433df69c2168
AvanesovVova <vova.avanesov.ads@gmail.com>
2024-06-30 01:37:28 +0400

    roadmap animation done

--- src/components/RoadmapWeb.tsx
+++ src/components/RoadmapWeb.tsx
@@ -77,7 +77,7 @@ function RoadmapWeb() {
     "FEBRUARY",
   ]);
 
-  
+
   const [count, setCount] = useState(0);
 
   useEffect(() => {
@@ -121,51 +121,51 @@ function RoadmapWeb() {
 
   return (
     <>
-      <div className="security-component-header">
-        <div className="line_1"></div>
-        <div className="security-component-title">check our</div>
-      </div>
-      <div className="subtitle">Roadmap</div>
-
-      <div className="roadmapBlocLine">
-        <img className="roadmapCircle" src={RoadmapDot} alt="" />
-      </div>
-      <div className="line_vertical">
-        <img className="roadmapCircleVertical" src={RoadmapDot} alt="" />
-      </div>
-
-      <img className="roadmapCircleVerticalSecondToThird" src={RoadmapDot} alt="" />
-
-      <div className="line_third_to_fifth"></div>
-      <img className="roadmapCircleVerticalThirdToFifth" src={RoadmapDot} alt="" />
-      <div className="line_fifth_to_fourth"></div>
-      <img className="roadmapCircleVerticalFifth" src={RoadmapDot} alt="" />
-      {/* <div className="line_next"></div> */}
-      {/* <span className="right_arrow"></span> */}
-      <svg className="line_next" xmlns="http://www.w3.org/2000/svg" width="147" height="9" viewBox="0 0 147 9" fill="none">
-        <path d="M146.354 4.85357C146.549 4.6583 146.549 4.34172 146.354 4.14646L143.172 0.964479C142.976 0.769216 142.66 0.769216 142.464 0.964479C142.269 1.15974 142.269 1.47632 142.464 1.67159L145.293 4.50001L142.464 7.32844C142.269 7.5237 142.269 7.84028 142.464 8.03555C142.66 8.23081 142.976 8.23081 143.172 8.03555L146.354 4.85357ZM-4.37114e-08 5L146 5.00001L146 4.00001L4.37114e-08 4L-4.37114e-08 5Z" fill="url(#paint0_linear_71_2238)" />
-        <defs>
-          <linearGradient id="paint0_linear_71_2238" x1="9.78919" y1="5.25" x2="9.79314" y2="4.25002" gradientUnits="userSpaceOnUse">
-            <stop stop-color="#75030B" />
-            <stop offset="0.333333" stop-color="#930210" />
-            <stop offset="0.666667" stop-color="#B4001E" />
-            <stop offset="1" stop-color="#DA291C" />
-          </linearGradient>
-        </defs>
-      </svg>
-
-      <span className="roadmapNextBtn" onClick={Next}>NEXT</span>
-
-
-      {roadmapContent.map((el, index) => (
-        <div key={index} id={blockId[index]} className={animClass[index]}>
-          <span className="roadmapTitle">
-            {title[index]}
-            <span className="smTitleRoadmap"> /2023</span>
-          </span>
+      <div className="roadmap">
+        <div className="security-component-header">
+          <div className="line_1"></div>
+          <div className="security-component-title">check our</div>
         </div>
-      ))}
+        <div className="subtitle">Roadmap</div>
 
+        <div className="roadmapBlocLine">
+          <img className="roadmapCircle" src={RoadmapDot} alt="" />
+        </div>
+        <div className="line_vertical">
+          <img className="roadmapCircleVertical" src={RoadmapDot} alt="" />
+        </div>
+
+        <img className="roadmapCircleVerticalSecondToThird" src={RoadmapDot} alt="" />
+
+        <div className="line_third_to_fifth"></div>
+        <img className="roadmapCircleVerticalThirdToFifth" src={RoadmapDot} alt="" />
+        <div className="line_fifth_to_fourth"></div>
+        <img className="roadmapCircleVerticalFifth" src={RoadmapDot} alt="" />
+        <svg className="line_next" xmlns="http://www.w3.org/2000/svg" width="147" height="9" viewBox="0 0 147 9" fill="none">
+          <path d="M146.354 4.85357C146.549 4.6583 146.549 4.34172 146.354 4.14646L143.172 0.964479C142.976 0.769216 142.66 0.769216 142.464 0.964479C142.269 1.15974 142.269 1.47632 142.464 1.67159L145.293 4.50001L142.464 7.32844C142.269 7.5237 142.269 7.84028 142.464 8.03555C142.66 8.23081 142.976 8.23081 143.172 8.03555L146.354 4.85357ZM-4.37114e-08 5L146 5.00001L146 4.00001L4.37114e-08 4L-4.37114e-08 5Z" fill="url(#paint0_linear_71_2238)" />
+          <defs>
+            <linearGradient id="paint0_linear_71_2238" x1="9.78919" y1="5.25" x2="9.79314" y2="4.25002" gradientUnits="userSpaceOnUse">
+              <stop stop-color="#75030B" />
+              <stop offset="0.333333" stop-color="#930210" />
+              <stop offset="0.666667" stop-color="#B4001E" />
+              <stop offset="1" stop-color="#DA291C" />
+            </linearGradient>
+          </defs>
+        </svg>
+
+        <span className="roadmapNextBtn" onClick={Next}>NEXT</span>
+        <span className="roadmapPrevBtn" onClick={animPrev}>PREV</span>
+
+
+        {roadmapContent.map((el, index) => (
+          <div key={index} id={blockId[index]} className={animClass[index]}>
+            <span className="roadmapTitle">
+              {title[index]}
+              <span className="smTitleRoadmap"> /2023</span>
+            </span>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
