420f2db991de516f83865c4bb01e0dfc3465eb70
AvanesovVova <vova.avanesov.ads@gmail.com>
2024-06-29 19:46:06 +0400

    change css roadmap

--- src/components/RoadmapWeb.tsx
+++ src/components/RoadmapWeb.tsx
@@ -4,55 +4,61 @@ import React, { useState } from "react";
 
 function swap() {
   const elementFirst = document.getElementById("first");
-    if (elementFirst) {
-      // Trigger the transition by adding the 'move' class
-      elementFirst.classList.add("moveLeft");
-    }
+  if (elementFirst) {
+    // Trigger the transition by adding the 'move' class
+    elementFirst.classList.add("moveLeft");
+  }
 
-    const elementSecond = document.getElementById("second");
-    if (elementSecond) {
-      // Trigger the transition by adding the 'move' class
-      elementSecond.classList.add("moveStart");
-    }
-   
-    const elementThird = document.getElementById("third");
-    if (elementThird) {
-      // Trigger the transition by adding the 'move' class
-      elementThird.classList.add("move");
-    }
+  const elementSecond = document.getElementById("second");
+  if (elementSecond) {
+    // Trigger the transition by adding the 'move' class
+    elementSecond.classList.add("moveStart");
+  }
 
-    const elementForth = document.getElementById("fourth");
-    if (elementForth) {
-      // Trigger the transition by adding the 'move' class
-      elementForth.classList.add("moveRight");
-    }
+  const elementThird = document.getElementById("third");
+  if (elementThird) {
+    // Trigger the transition by adding the 'move' class
+    elementThird.classList.add("move");
+  }
+
+  const elementForth = document.getElementById("fourth");
+  if (elementForth) {
+    // Trigger the transition by adding the 'move' class
+    elementForth.classList.add("moveRight");
+  }
 
 
-    const elementFourth = document.getElementById("forth");
-    if (elementFourth) {
-        // Trigger the transition by adding the 'move' class
-        elementFourth.classList.add("moveRight");
-      }
+  const elementFourth = document.getElementById("forth");
+  if (elementFourth) {
+    // Trigger the transition by adding the 'move' class
+    elementFourth.classList.add("moveRight");
+  }
 
-      const elementFifth = document.getElementById("fifth");
-      if (elementFifth) {
-          // Trigger the transition by adding the 'move' class
-          elementFifth.classList.add("moveDown");
-        }
-}   
+  const elementFifth = document.getElementById("fifth");
+  if (elementFifth) {
+    // Trigger the transition by adding the 'move' class
+    elementFifth.classList.add("moveDown");
+  }
+}
 
 function RoadmapWeb() {
-    return (
-        
-      <div className="roadmapWeb">
-              <button onClick={swap}>NEXT</button>
+  return (
+    <>
+<div className="security-component-header">
+                    <div className="line_1"></div>
+                    <div className="security-component-title">check our</div>
+                </div>
+                <div className="subtitle">Roadmap</div>
+
+                <div className="roadmapBlocLine"></div>
+      {/* <button onClick={swap}>NEXT</button>
                 <div id="first">first</div>
                 <div id="second">second</div>
                 <div id="third">third</div>
                 <div id="fourth">fourth</div>
-                <div id="fifth">fifth</div>
-            </div>
-    )
+                <div id="fifth">fifth</div> */}
+    </>
+  )
 }
 
 export default RoadmapWeb
