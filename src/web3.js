0c30a490a51726e8bab36bd91141668cdba2fc15
AvanesovVova <vova.avanesov.ads@gmail.com>
2024-07-17 14:01:06 +0400

    change

--- src/web3.js
+++ src/web3.js
@@ -1,6 +1,6 @@
 import Web3 from 'web3';
-import { ABI } from '../src/ABI';
-import { marketABI } from './marketABI';
+import { ABI } from './ABI/ABI';
+import { marketABI } from './ABI/marketABI';
 
 // Binance Smart Chain testnet RPC URL
 const bscTestnetProvider = 'https://data-seed-prebsc-1-s1.binance.org:8545';
@@ -8,7 +8,7 @@ const web3 = new Web3(new Web3.providers.HttpProvider(bscTestnetProvider));
 
 // Test contract address on BSC testnet
 const RedCostNFTAddress = "0x6f07f1C47a1A27b7d27361a5e726e8438CD0CE1c";
-const RedCostMarketAddress= "0x61Bf9d884aee55c86964254CbC52B6987C01bD06"
+const RedCostMarketAddress= "0x503aE66598B1c2246f9592EAA59dC5b121e4fC43"
 
 const contractNFT = new web3.eth.Contract(ABI, RedCostNFTAddress);
 const contractMarket = new web3.eth.Contract(marketABI, RedCostMarketAddress);
@@ -46,5 +46,20 @@ export async function _decreaseAuctionPrice(_account,_tokenID) {
         return dropTheRate;
     } catch (error) {
         console.error("Error DropTheRate:", error);
+    }
+}
+
+export async function _buy (_tokenID){
+    try {
+        // Estimate gas for the transaction
+        const estimatedGas = await contractMarket.methods.buy(_tokenID).estimateGas({ from: _account });
+        let buyNFT = await contractMarket.methods.buy(_tokenID).send({
+            from: _account,
+            gas: estimatedGas, // Set the estimated gas limit
+            gasPrice: gasPrice, // Set the current gas price
+        });
+        return buyNFT;
+    } catch (error) {
+        console.error("Error Buy:", error);
     }
 }
